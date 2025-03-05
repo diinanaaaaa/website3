42a09c7e23637cf531b6b76d851df0a711fe41a7
diinanaaaaa <nurdiyanahnabila@gmail.com>
2025-03-05 15:18:12 +0800

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 
 function toggleMenu() {
-    document.querySelector(".navigation").classList.toggle("active");
-    document.querySelector(".menu-toggle").classList.toggle("open");
-}
+    let nav = document.querySelector(".navigation");
+    let toggle = document.querySelector(".menu-toggle");
 
+    nav.classList.toggle("active");
+    toggle.classList.toggle("open");
+
+    if (nav.classList.contains("active")) {
+        nav.style.display = "flex";
+    } else {
+        nav.style.display = "none";
+    }
+}
 
